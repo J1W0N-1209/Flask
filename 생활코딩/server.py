c4b21f261a66654a99caeaf15775db3cdd40040e
jiwonKang <nabook754@naver.com>
2022-11-06 13:34:19 +0900

    Delete

--- 생활코딩/server.py
+++ 생활코딩/server.py
@@ -15,6 +15,7 @@ def template(contents,content,id=None):
     if id != None:
         contextUI = f'''
             <li><a href="/update/{id}/">update</a></li>
+            <li><form action="/delete/{id}/" method="POST" ><input type="submit" value="delete"></form></li>
         '''
 
     return f'''
@@ -108,5 +109,13 @@ def read(id):
             body = topic['body']
             break
     return template(getContents(),f'<h2>{title}</h2>{body}',id)
+
+@app.route('/delete/<int:id>/',methods=['POST'])
+def delete(id):
+    for topic in topics:
+        if id == topic['id']:
+            topics.remove(topic)
+            break
+    return redirect('/')
 
 app.run(debug=True)
